6340fe03073a1bc975697b10bc368bfdd9620468
Michael Friis <friism@gmail.com>
2016-11-19 10:04:43 -0800

    set error setting

--- specialize-script.ps1
+++ specialize-script.ps1
@@ -12,5 +12,9 @@ Pop-Location
 
 $keyPath = "C:\Users\Administrator\.ssh\authorized_keys"
 $keyUrl = "http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key"
+
 New-Item -ErrorAction Ignore -Type Directory C:\Users\Administrator\.ssh
+
+$ErrorActionPreference = 'SilentlyContinue'
 Do { Start-Sleep 1 ; Invoke-WebRequest $keyUrl -UseBasicParsing -OutFile $keyPath } While ( -Not (Test-Path $keyPath) )
+$ErrorActionPreference = 'Stop'
