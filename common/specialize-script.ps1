93ad3407d2082c2680c5ac95b6d34f1e0d58942a
Michael Friis <friism@gmail.com>
2016-12-04 15:18:32 -0800

    kill remote desktop since ec2 script enables it

--- common/specialize-script.ps1
+++ common/specialize-script.ps1
@@ -25,4 +25,6 @@ Do {
 } While ( -Not (Test-Path $keyPath) )
 $ErrorActionPreference = 'Stop'
 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+
 Stop-Transcript
