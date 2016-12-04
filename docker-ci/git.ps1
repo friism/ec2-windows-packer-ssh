f89719a308a1a507ade0b3fbbf4a9a88bff43258
Michael Friis <friism@gmail.com>
2016-12-04 15:19:33 -0800

    use git 2.11

--- docker-ci/git.ps1
+++ docker-ci/git.ps1
@@ -1,7 +1,7 @@
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
 
-Invoke-Webrequest "https://github.com/git-for-windows/git/releases/download/v2.10.2.windows.1/Git-2.10.2-64-bit.exe" -OutFile git.exe -UseBasicParsing
+Invoke-Webrequest "https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/Git-2.11.0-64-bit.exe" -OutFile git.exe -UseBasicParsing
 Start-Process git.exe -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /DIR=C:\git' -Wait
 Remove-Item -Force git.exe
 
