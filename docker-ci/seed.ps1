b0c2e0f629e413347d5d58e72b662c58d53f6596
Michael Friis <friism@gmail.com>
2016-12-22 18:40:32 -0800

    upgrade docker

--- docker-ci/seed.ps1
+++ docker-ci/seed.ps1
@@ -6,7 +6,7 @@ Start-Transcript -path ("C:\{0}.log" -f $MyInvocation.MyCommand.Name) -append
 Stop-Service docker
 
 Remove-Item -Force -Recurse $Env:ProgramFiles\docker
-Invoke-WebRequest https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc3.zip -UseBasicParsing -OutFile docker.zip
+Invoke-WebRequest https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc4.zip -UseBasicParsing -OutFile docker.zip
 Expand-Archive docker.zip -DestinationPath $Env:ProgramFiles
 Remove-Item -Force docker.zip
 
