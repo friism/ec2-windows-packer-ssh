b51e40559ec0a52ed9d4ee2ba94f9f2f60626294
Michael Friis <friism@gmail.com>
2017-01-06 10:58:57 -0800

    disable windows update

--- common/button-down.ps1
+++ common/button-down.ps1
@@ -18,11 +18,14 @@ $services = @(
 	"TrkWks"
 	"WbioSrvc"
 	"XblAuthManager"
-	"XblGameSave"
+	"XblGameSave",
+	"wuauserv"
 )
 foreach ($service in $services) {
 	Get-Service -Name $service | Set-Service -StartupType Disabled
 }
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 
 schtasks /End /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
 schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
