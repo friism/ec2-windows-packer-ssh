a790fd3af345b507a36d69341b47bc05ab101936
Michael Friis <friism@gmail.com>
2017-02-05 18:30:29 -0800

    add docker-compose

--- docker-ci/docker.ps1
+++ docker-ci/docker.ps1
@@ -12,6 +12,8 @@ Expand-Archive docker.zip -DestinationPath $Env:ProgramFiles
 Remove-Item -Force docker.zip
 Remove-Item -Force -Recurse $Env:ProgramFiles\docker\completion
 
+Invoke-WebRequest https://github.com/docker/compose/releases/download/1.10.1/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
+
 # REMARK: http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/windows-ami-version-history.html#win2k16-amis
 dockerd -H npipe:////./pipe/docker_engine -H 0.0.0.0:2375 --experimental --register-service
 
