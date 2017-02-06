d2413e04466d50aa428c290e960d3741e9d0531a
Michael Friis <friism@gmail.com>
2017-02-05 18:29:54 -0800

    upgrade docker and git

--- docker-ci/git.ps1
+++ docker-ci/git.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 Start-Transcript -path ("C:\{0}.log" -f $MyInvocation.MyCommand.Name) -append
 
-Invoke-Webrequest "https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/Git-2.11.0-64-bit.exe" -OutFile git.exe -UseBasicParsing
+Invoke-Webrequest "https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/Git-2.11.1-64-bit.exe" -OutFile git.exe -UseBasicParsing
 Start-Process git.exe -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /DIR=C:\git' -Wait
 Remove-Item -Force git.exe
 
