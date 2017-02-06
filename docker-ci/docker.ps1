fa7671d5b21b59687295322cb6775d8b432337f3
Michael Friis <friism@gmail.com>
2017-02-05 18:29:54 -0800

    upgrade docker and git

--- docker-ci/docker.ps1
+++ docker-ci/docker.ps1
@@ -7,13 +7,13 @@ Stop-Service -Force docker
 dockerd --unregister-service
 
 Remove-Item -Force -Recurse $Env:ProgramFiles\docker
-Invoke-WebRequest https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc7.zip -UseBasicParsing -OutFile docker.zip
+Invoke-WebRequest https://test.docker.com/builds/Windows/x86_64/docker-1.13.1-rc1.zip -UseBasicParsing -OutFile docker.zip
 Expand-Archive docker.zip -DestinationPath $Env:ProgramFiles
 Remove-Item -Force docker.zip
+Remove-Item -Force -Recurse $Env:ProgramFiles\docker\completion
 
 # REMARK: http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/windows-ami-version-history.html#win2k16-amis
-dockerd -H npipe:////./pipe/docker_engine -H 0.0.0.0:2375 --fixed-cidr "172.17.0.0/16" --register-service
-sc.exe config docker start= delayed-auto
+dockerd -H npipe:////./pipe/docker_engine -H 0.0.0.0:2375 --experimental --register-service
 
 Start-Service docker
 
