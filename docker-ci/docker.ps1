8cfa08034d9b82796b3902f9e6130ec13c5c6610
Michael Friis <friism@gmail.com>
2017-02-07 08:15:01 -0800

    parametrize docker version

--- docker-ci/docker.ps1
+++ docker-ci/docker.ps1
@@ -1,3 +1,10 @@
+Param(
+	[Parameter(Mandatory=$True)]
+	[string]$dockerVersion,
+	[Parameter(Mandatory=$True)]
+	[string]$dockerComposeVersion
+)
+
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
 
@@ -7,12 +14,12 @@ Stop-Service -Force docker
 dockerd --unregister-service
 
 Remove-Item -Force -Recurse $Env:ProgramFiles\docker
-Invoke-WebRequest https://test.docker.com/builds/Windows/x86_64/docker-1.13.1-rc2.zip -UseBasicParsing -OutFile docker.zip
+Invoke-WebRequest "https://test.docker.com/builds/Windows/x86_64/docker-$dockerVersion.zip" -UseBasicParsing -OutFile docker.zip
 Expand-Archive docker.zip -DestinationPath $Env:ProgramFiles
 Remove-Item -Force docker.zip
 Remove-Item -Force -Recurse $Env:ProgramFiles\docker\completion
 
-Invoke-WebRequest https://github.com/docker/compose/releases/download/1.10.1/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 
 # REMARK: http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/windows-ami-version-history.html#win2k16-amis
 dockerd -H npipe:////./pipe/docker_engine -H 0.0.0.0:2375 --experimental --register-service
